deeda466da98aacbec57da81f058d596807e56cb
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2022-01-20 11:37:58 -0300

    feat: auto generate CRUD of product

---
+++ src/models/product.model.ts
@@ -0,0 +1,32 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Product extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+  })
+  nome?: string;
+
+  @property({
+    type: 'number',
+  })
+  preco?: number;
+
+  constructor(data?: Partial<Product>) {
+    super(data);
+  }
+}
+
+export interface ProductRelations {
+  // describe navigational properties here
+}
+
+export type ProductWithRelations = Product & ProductRelations;
